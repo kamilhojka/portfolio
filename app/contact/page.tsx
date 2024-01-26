024805894e7aa78f70bc9b4f7cbad0d39cf9ca9e
Kamil Hojka <kamhojka@gmail.com>
2024-01-26 20:21:14 +0100

    Enhance styling due to different dimensions of screen

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
 export default function ContactPage() {
-  return <div className="flex flex-1">Contact page</div>;
+  return (
+    <div className="flex flex-1 flex-col w-full sm:px-12 gap-12 py-12">
+      <Link href="/">
+        <Button variant="default" className="w-20">
+          Home
+        </Button>
+      </Link>
+      Contact page
+    </div>
+  );
 }
--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,16 +1,16 @@
 export function Hero() {
   return (
-    <section
-      id="hero"
-      className="relative flex flex-col py-0 sm:py-6 md:py-8 lg:py-12"
-    >
-      <h2 className="text-3xl md:text-4xl font-medium tracking-tighter gap-2">
-        Hi, my name is Kamil
-        <span className="font-black text-primary">.</span>
-      </h2>
-      <h1 className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-bold font-serif tracking-tighter -mt-2">
-        I&apos;m a <span className="text-primary">Full Stack Developer</span>
-      </h1>
+    <section id="hero" className="relative flex flex-col py-12">
+      <div className="flex flex-col gap-4">
+        <h2 className="text-3xl md:text-4xl font-medium tracking-tighter">
+          Hi, my name is Kamil
+          <span className="font-black text-primary">.</span>
+        </h2>
+        <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold font-serif tracking-tighter -mt-2 leading-10">
+          I&apos;m a <span className="text-primary">Full Stack Developer</span>
+          <span className="font-black">.</span>
+        </h1>
+      </div>
       <p className="max-w-2xl text-lg text-muted-foreground text-justify pt-8">
         I&apos;ve spent the last 4 years building, managing and scaling
         softwares for company in the furniture industry. After graduation, I am
