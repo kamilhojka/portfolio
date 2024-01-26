c4a5252020fa4e678689de8851fddb338c6d37e8
Kamil Hojka <kamhojka@gmail.com>
2024-01-26 17:20:43 +0100

    Add 'About' section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { About } from "@/components/sections/about";
 import { Footer } from "@/components/sections/footer";
 import { Hero } from "@/components/sections/hero";
 
@@ -5,7 +6,9 @@ export default function Home() {
   return (
     <div className="container relative min-h-screen flex flex-col gap-12 w-full">
       <Hero />
-      <main className="flex flex-1"></main>
+      <main className="flex flex-1 flex-col w-full px-6 sm:px-12 pb-12 gap-12">
+        <About />
+      </main>
       <Footer />
     </div>
   );
