8d1bc85342a9d826ec63183d90fd7c6e53d5b267
Kamil Hojka <kamhojka@gmail.com>
2024-01-26 17:04:12 +0100

    Add 'Footer' component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Footer } from "@/components/sections/footer";
 import { Hero } from "@/components/sections/hero";
 
 export default function Home() {
   return (
     <div className="container relative min-h-screen flex flex-col gap-12 w-full">
       <Hero />
+      <main className="flex flex-1"></main>
+      <Footer />
     </div>
   );
 }
---
+++ components/sections/footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="flex flex-col sm:flex-row rounded-t-3xl border-t p-12 justify-between items-start sm:items-center gap-4 bg-[#9fbbf7]">
+      <h2 className="text-4xl font-medium tracking-tight text-primary-foreground">
+        <span className="font-semibold">© 2024</span> Kamil Hojka
+      </h2>
+    </footer>
+  );
+}
