95027bf8ff735460f098139898a62487d5375b75
Kamil Hojka <kamhojka@gmail.com>
2024-01-26 17:34:23 +0100

    Enhance 'SectionHeading' component variants

--- components/common/section-heading.tsx
+++ components/common/section-heading.tsx
@@ -1,13 +1,38 @@
-interface SectionHeadingProps {
+import { VariantProps, cva } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const sectionHeadingVariants = cva(
+  "flex items-end text-6xl lg:text-7xl font-bold font-serif",
+  {
+    variants: {
+      justify: {
+        start: "justify-start",
+        end: "justify-end",
+      },
+    },
+    defaultVariants: {
+      justify: "start",
+    },
+  }
+);
+
+interface SectionHeadingProps
+  extends VariantProps<typeof sectionHeadingVariants> {
   title: string;
   id?: string;
   children: React.ReactNode;
 }
 
-export function SectionHeading({ title, id, children }: SectionHeadingProps) {
+export function SectionHeading({
+  title,
+  id,
+  justify,
+  children,
+}: SectionHeadingProps) {
   return (
     <section id={id} className="flex flex-col gap-8">
-      <h2 className="flex items-end text-6xl lg:text-7xl font-bold font-serif">
+      <h2 className={cn(sectionHeadingVariants({ justify }))}>
         {title}
         <span className="block rounded-full w-3 h-3 bg-blue-300" />
       </h2>
