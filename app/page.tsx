af7ef5192c027df09459e811fe54e58a5001801e
Kamil Hojka <kamhojka@gmail.com>
2024-01-29 10:23:20 +0100

    Change the style of main in 'app/page.tsx'

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Projects } from "@/components/sections/projects";
 
 export default function Home() {
   return (
-    <main className="flex flex-1 flex-col w-full sm:px-12 gap-12">
+    <main className="flex flex-1 flex-col w-full pt-6 sm:px-12 gap-24">
       <Hero />
       <About />
       <Projects />
