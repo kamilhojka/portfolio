eb3168205101ea4fdfc2fe3fb15c669f87a9af51
Kamil Hojka <kamhojka@gmail.com>
2024-01-29 17:13:26 +0100

    Add 'resume.pdf' and edit 'ResumeButton' to navigate to it

--- components/common/resume-button.tsx
+++ components/common/resume-button.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
 import { IoDocumentText } from "react-icons/io5";
+
 import { Button } from "@/components/ui/button";
 
 export function ResumeButton() {
   return (
-    <Button className="gap-2 text-md" variant="glossy-outline">
-      <IoDocumentText className="h-[1.2rem] w-[1.2rem]" /> Resume
-    </Button>
+    <Link href="/resume.pdf" passHref target="_blank">
+      <Button className="gap-2 text-md" variant="glossy-outline">
+        <IoDocumentText className="h-[1.2rem] w-[1.2rem]" /> Resume
+      </Button>
+    </Link>
   );
 }
