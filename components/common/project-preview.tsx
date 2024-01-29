ea38351efb638415463c17e11d6bf2f1a2aab0ad
Kamil Hojka <kamhojka@gmail.com>
2024-01-29 14:28:03 +0100

    Update content and 'ProjectPreview' component

--- components/common/project-preview.tsx
+++ components/common/project-preview.tsx
@@ -8,13 +8,13 @@ export function PostPreview(props: ProjectMetadata) {
   return (
     <Link href={`/projects/${props.slug}`}>
       <div className="relative h-[200px] lg:h-fit overflow-hidden rounded-md border-2 dark:border-primary-foreground hover:dark:border-primary hover:border-primary group transition-all duration-800 ease-in-out">
-        <div className="absolute inset-0 -z-10 lg:static">
+        <div className="absolute inset-0 -z-10 lg:relative">
           <Image
             src={props.image}
             alt={props.imageAlt}
-            className="object-cover object-center opacity-30 dark:opacity-10 transition-all duration-800 ease-in-out grayscale lg:group-hover:opacity-100 group-hover:opacity-20 group-hover:grayscale-0"
+            className="object-fill object-center opacity-30 dark:opacity-10 transition-all duration-800 ease-in-out grayscale lg:group-hover:opacity-100 group-hover:opacity-20 group-hover:grayscale-0"
             width={1000}
-            height={2000}
+            height={1000}
           />
           <Badge
             className="absolute inset-2 w-fit h-fit"
@@ -24,7 +24,7 @@ export function PostPreview(props: ProjectMetadata) {
           </Badge>
         </div>
         <div className="z-10 flex flex-col p-4 h-full justify-end lg:justify-center">
-          <h2 className="font-serif text-3xl font-bold text-foreground mb-2">
+          <h2 className="font-serif text-2xl font-bold text-foreground tracking-tight leading-6 mb-2">
             {props.title}
           </h2>
           <div className="flex flex-wrap gap-2">
---
+++ content/furniture-detection-app.md
@@ -0,0 +1,18 @@
+---
+slug: "furniture-detection-app"
+title: "Furniture Detection App"
+purpose: "University"
+technologies: ["Python", "YOLOv8", "Streamlit"]
+liveUrl: ""
+githubUrl: "https://github.com/kamilhojka/furniture-detection-yolov8-streamlit"
+image: "/assets/content/furniture-detection-app/image-dark.png"
+imageAlt: "furniture-detection-app thumbnail"
+---
+
+As part of a diploma thesis, an application was created using a deep learning model achieving 92% accuracy in detecting objects within room interiors based on user-uploaded images. This innovation holds potential for streamlining processes across industries.
+
+The thesis aimed to design an application for object detection within room interiors using a deep learning model. Technologies like Visual Studio Code, Python, venv, Git, Jupyter Notebook, YOLOv8, Streamlit, and Roboflow were chosen. YOLOv8, a potent framework for object detection, was selected for model creation. Diverse datasets (Open Images Dataset V7, Bonn Furniture Styles Dataset) depicted common room objects.
+
+The project successfully designed an object detection app, showcasing a 92% accuracy rate. This innovative solution holds potential for diverse applications, revolutionizing industry processes and user experience. The chosen technologies, especially YOLOv8 and Streamlit, contribute to a robust and efficient application.
+
+![Application](/assets/content/furniture-detection-app/image.png)
