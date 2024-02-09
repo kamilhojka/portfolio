ca1974544e71bb610cc4ac60aa9c35718d12a2a5
Kamil Hojka <hello@kamilhojka.com>
2024-02-09 19:10:13 +0100

    Remove unused imports and commented code in 'CustomCursor' component

--- components/common/custom-cursor.tsx
+++ components/common/custom-cursor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import { cn } from "@/lib/utils";
 
@@ -38,5 +38,3 @@ export function CustomCursor() {
     ></div>
   );
 }
-
-// ${isPointer ? "pointer" : ""}
